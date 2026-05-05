60970e6bca5fde75de3ffec59e4bd6f7dd756959
Качановський Владислав bc2025-2 <kacanovskijvlad@gmail.com>
2026-05-05 22:12:12 +0300

    feat: connect inventory API to fetch gallery data

--- src/pages/Gallery.jsx
+++ src/pages/Gallery.jsx
@@ -1,13 +1,28 @@
+import { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
+import { inventoryApi } from '../services/inventoryApi';
 
 export default function Gallery() {
+  const [items, setItems] = useState([]);
+
+  useEffect(() => {
+    inventoryApi.getAll()
+      .then(data => setItems(data))
+      .catch(err => console.error(err));
+  }, []);
+
   return (
     <div>
       <h1>Каталог інвентарю</h1>
       <nav>
         <Link to="/favorites">Улюблені</Link> | <Link to="/admin">Адмін-панель</Link>
       </nav>
-      <p>Тут буде список інвентарю</p>
+      <hr />
+      {items.map(item => (
+        <div key={item.id}>
+          {item.inventory_name}
+        </div>
+      ))}
     </div>
   );
 }
