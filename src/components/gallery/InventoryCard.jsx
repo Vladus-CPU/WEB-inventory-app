64d0ce2779c163a08e24956b07ddeb66a339a175
Качановський Владислав bc2025-2 <kacanovskijvlad@gmail.com>
2026-05-05 23:07:16 +0300

    feat: add display images in cards

--- src/components/gallery/InventoryCard.jsx
+++ src/components/gallery/InventoryCard.jsx
@@ -4,6 +4,11 @@ import styles from './InventoryCard.module.css';
 export default function InventoryCard({ item }) {
   return (
     <div className={styles.card}>
+    <img
+    src={item.photo || ''}
+    alt={item.inventory_name}
+    className={styles.image}
+    />
       <h3 className={styles.title}>{item.inventory_name}</h3>
     </div>
   );
